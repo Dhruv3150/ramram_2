bd932678c87b361885d20799e773710de33b8add
Dhruv3150 <khatranidhruv@gmail.com>
2023-09-12 17:42:19 +0530

    first command

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Contact = () => {
+  const [data, setdata] = useState({ name: "", email: "", message: "" });
+
+  const handleInput = (e) => {
+    setdata({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    console.log("hello");
+  }, [data]);
+
   return (
     <div className="Contact">
       <main>
         <h1>Contact Us</h1>
 
-        <form action="">
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div>
             <label>Name</label>
-            <input type="text" required placeholder="Abc" />
+            <input
+              type="text"
+              name="name"
+              value={data.name}
+              onChange={(e) => handleInput(e)}
+              required
+              placeholder="Abc"
+            />
           </div>
 
           <div>
             <label>Email</label>
-            <input type="Email" required placeholder="yxz@gmail.com" />
+            <input
+              type="Email"
+              name="email"
+              value={data.email}
+              onChange={(e) => handleInput(e)}
+              required
+              placeholder="yxz@gmail.com"
+            />
           </div>
 
           <div>
             <label>Message</label>
             <input
               type="text"
+              name="message"
+              value={data.message}
+              onChange={(e) => handleInput(e)}
               required
               placeholder="Tell us About Your Query.."
             />
